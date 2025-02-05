30c7a9760cd0976e316e61b65efe31df3cc1fa3b
Akhmad Taufik Ismail <akhmadtaufik705@gmail.com>
2025-02-05 13:38:06 +0700

    refactor: migrate songs module to new structure

---
+++ src/api/songs/handler.js
@@ -0,0 +1,90 @@
+/* eslint-disable no-unused-vars */
+const ClientError = require('../../exceptions/ClientError');
+
+class SongsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postSongHandler = this.postSongHandler.bind(this);
+    this.getAllSongsHandler = this.getAllSongsHandler.bind(this);
+    this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
+    this.putSongByIdHandler = this.putSongByIdHandler.bind(this);
+    this.deleteSongByIdHandler = this.deleteSongByIdHandler.bind(this);
+  }
+
+  async postSongHandler(request, h) {
+    this._validator.validateSongPayload(request.payload);
+    const { title, year, genre, performer, duration, albumId } =
+      request.payload;
+
+    const songId = await this._service.addSong({
+      title,
+      year,
+      genre,
+      performer,
+      duration,
+      albumId,
+    });
+
+    return h
+      .response({
+        status: 'success',
+        data: { songId },
+      })
+      .code(201);
+  }
+
+  async getAllSongsHandler(request) {
+    const { title, performer } = request.query;
+    const songs = await this._service.getSongs(title, performer);
+
+    return {
+      status: 'success',
+      data: { songs },
+    };
+  }
+
+  async getSongByIdHandler(request, h) {
+    const { id } = request.params;
+    const song = await this._service.getSongById(id);
+
+    return {
+      status: 'success',
+      data: { song },
+    };
+  }
+
+  async putSongByIdHandler(request, h) {
+    this._validator.validateSongPayload(request.payload);
+    const { id } = request.params;
+    const { title, year, genre, performer, duration, albumId } =
+      request.payload;
+
+    await this._service.editSongById(id, {
+      title,
+      year,
+      genre,
+      performer,
+      duration,
+      albumId,
+    });
+
+    return {
+      status: 'success',
+      message: 'Lagu berhasil diperbaharui',
+    };
+  }
+
+  async deleteSongByIdHandler(request, h) {
+    const { id } = request.params;
+    await this._service.deleteSongById(id);
+
+    return {
+      status: 'success',
+      message: 'Lagu berhasil dihapus',
+    };
+  }
+}
+
+module.exports = SongsHandler;
