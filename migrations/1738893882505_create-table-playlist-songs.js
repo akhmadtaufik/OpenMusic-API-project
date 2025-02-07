d1845e9ebcf4da7f33b5278547afa213612a882e
Akhmad Taufik Ismail <akhmadtaufik705@gmail.com>
2025-02-07 09:17:50 +0700

    migrations: create playlists and playlist_songs tables - add playlists table with id, name, and owner (FK to users) - add playlist_songs table with playlist_id (FK to playlists) and song_id (FK to songs) - enforce unique constraint on playlist_id and song_id

---
+++ migrations/1738893882505_create-table-playlist-songs.js
@@ -0,0 +1,43 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('playlist_songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: '"playlists"',
+      onDelete: 'CASCADE',
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: '"songs"',
+      onDelete: 'CASCADE',
+    },
+  });
+
+  pgm.addConstraint('playlist_songs', 'unique_playlist_song', {
+    unique: ['playlist_id', 'song_id'],
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('playlist_songs');
+};
