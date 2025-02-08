b0cd13008e25cd11c9693ab638b74ed095d26a53
Akhmad Taufik Ismail <akhmadtaufik705@gmail.com>
2025-02-08 12:23:16 +0700

    feat(playlists): add route for retrieving playlist activities - update routes.js to include GET /playlists/{id}/activities - modify index.js to pass playlistActivitiesService to handler

--- src/api/playlists/index.js
+++ src/api/playlists/index.js
@@ -4,10 +4,14 @@ const routes = require('./routes');
 module.exports = {
   name: 'playlists',
   version: '1.0.0',
-  register: async (server, { playlistsService, songsService, validator }) => {
+  register: async (
+    server,
+    { playlistsService, songsService, playlistActivitiesService, validator }
+  ) => {
     const handler = new PlaylistsHandler(
       playlistsService,
       songsService,
+      playlistActivitiesService,
       validator
     );
     server.route(routes(handler));
