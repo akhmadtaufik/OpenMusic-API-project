6ccd17aad5c3037c9b7dd5c748e3f46dfe585ca9
Akhmad Taufik Ismail <akhmadtaufik705@gmail.com>
2025-02-14 19:53:28 +0700

    feat(uploads): add album cover upload API - create route for uploading album covers - configure payload handling for multipart form-data - enforce max file size of 512KB with stream output

---
+++ src/api/uploads/routes.js
@@ -0,0 +1,18 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/albums/{id}/covers',
+    handler: handler.postUploadCoverHandler,
+    options: {
+      payload: {
+        allow: 'multipart/form-data',
+        multipart: true,
+        maxBytes: 512000,
+        output: 'stream',
+      },
+      auth: 'openmusic_jwt',
+    },
+  },
+];
+
+module.exports = routes;
