9c67f4df983677cf999bc77198dbec8ce3ba733b
akhmadtaufik <akhmadtaufik1703@gmail.com>
2025-02-28 08:11:50 +0700

    refactor(auth): remove unused import and enforce consistency
    - Removed unused 'AuthenticationError' import as it is no longer used
    - Standardized query formatting to use double quotes for consistency

--- src/service/AuthenticationsService.js
+++ src/service/AuthenticationsService.js
@@ -1,7 +1,5 @@
-/* eslint-disable no-unused-vars */
-const { Pool } = require('pg');
-const InvariantError = require('../exceptions/InvariantError');
-const AuthenticationError = require('../exceptions/AuthenticationError');
+const { Pool } = require("pg");
+const InvariantError = require("../exceptions/InvariantError");
 
 class AuthenticationsService {
   constructor() {
@@ -10,33 +8,33 @@ class AuthenticationsService {
 
   async addRefreshToken(token) {
     const query = {
-      text: 'INSERT INTO authentications VALUES($1)',
+      text: "INSERT INTO authentications VALUES($1)",
       values: [token],
     };
 
-    try {
-      await this._pool.query(query);
-    } catch (error) {
-      throw new InvariantError('Refresh token gagal ditambahkan');
+    const result = await this._pool.query(query);
+
+    if (result.rowCount === 0) {
+      throw new InvariantError("Refresh token gagal ditambahkan");
     }
   }
 
   async verifyRefreshToken(token) {
     const query = {
-      text: 'SELECT token FROM authentications WHERE token = $1',
+      text: "SELECT token FROM authentications WHERE token = $1",
       values: [token],
     };
 
     const result = await this._pool.query(query);
 
     if (!result.rows.length) {
-      throw new InvariantError('Refresh token tidak valid');
+      throw new InvariantError("Refresh token tidak valid");
     }
   }
 
   async deleteRefreshToken(token) {
     const query = {
-      text: 'DELETE FROM authentications WHERE token = $1',
+      text: "DELETE FROM authentications WHERE token = $1",
       values: [token],
     };
 
